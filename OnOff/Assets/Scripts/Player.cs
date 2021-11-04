3cc1a84365ba7b0c31496c51b7d62815d63cd604
evanAndersonDigipen <71675591+evanAndersonDigipen@users.noreply.github.com>
2021-11-04 16:27:45 -0700

    PHYSICS PLAYER BB

--- OnOff/Assets/Scripts/Player.cs
+++ OnOff/Assets/Scripts/Player.cs
@@ -7,32 +7,49 @@ public class Player : MonoBehaviour
     public float speed;
     public GameObject select;
     public bool holding;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
+        Vector3 pos = rb.velocity;
         if (Input.GetKey(KeyCode.W))
         {
-            pos.y += speed;
+            pos.y = speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            pos.x -= speed;
+            pos.x = -speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            pos.y -= speed;
+            pos.y = -speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            pos.x += speed;
+            pos.x = speed;
         }
-        transform.position = pos;
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            pos.y = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            pos.x = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            pos.y = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            pos.x = 0;
+        }
+        rb.velocity = pos;
     }
 }
