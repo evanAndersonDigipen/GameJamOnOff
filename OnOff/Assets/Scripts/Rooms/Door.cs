4d40d01e877238f46dd42d33d35a687a2b55d85e
evanAndersonDigipen <71675591+evanAndersonDigipen@users.noreply.github.com>
2021-11-09 15:51:03 -0800

    FIXED GAME BREAK AAAAAAAAAAAA

--- OnOff/Assets/Scripts/Rooms/Door.cs
+++ OnOff/Assets/Scripts/Rooms/Door.cs
@@ -8,7 +8,7 @@ public class Door : MonoBehaviour
     public typeOfDoor TypeOfDoor;
     public GameObject prefab;
 
-    Rooms rooms;
+    
     public GameObject g;
 
     [SerializeField]
@@ -21,11 +21,11 @@ public class Door : MonoBehaviour
     {
         g = Instantiate(prefab, gameObject.transform);
         if(TypeOfDoor == typeOfDoor.exit) g.transform.localPosition = new Vector3(-.5f, -.5f); 
-        else g.transform.localPosition = new Vector3(-.5f, 1);
+        else g.transform.localPosition = new Vector3(-.5f, 1.3f);
 
         g.transform.localScale *= .25f;
 
-        rooms = GetComponent<Rooms>();
+        
 
         /*foreach(KeyValuePair<int, bool> keyValuePair in rooms.roomDict)
         {
@@ -39,7 +39,7 @@ public class Door : MonoBehaviour
         if (TypeOfDoor == typeOfDoor.exit)
         {
             //rooms.value = g.GetComponent<IO>().value;
-            if (rooms.value)
+            if (g.GetComponent<IO>().value)
             {
                 GetComponent<SpriteRenderer>().sprite = sprites[1];
                 GetComponent<Collider2D>().isTrigger = true;
@@ -62,11 +62,11 @@ public class Door : MonoBehaviour
         {
             //g.transform.localPosition = new Vector3(g.transform.localPosition.x, .5f);
 
-            int temp = PlayerPrefs.GetInt((rooms.id - 1).ToString());
-            g.GetComponent<IO>().value = System.Convert.ToBoolean(PlayerPrefs.GetInt((rooms.id-1).ToString()));
+            //int temp = PlayerPrefs.GetInt((rooms.id - 1).ToString());
+            g.GetComponent<IO>().value = true;
             
         }
-        Debug.Log(g.GetComponent<IO>().value);
+        //Debug.Log(g.GetComponent<IO>().value);
         
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -75,11 +75,11 @@ public class Door : MonoBehaviour
         {
             if(TypeOfDoor == typeOfDoor.exit)
             {
-                SceneManager.LoadScene(rooms.id + 1);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
             else
             {
-                SceneManager.LoadScene(rooms.id - 1);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
             }
             
         }
